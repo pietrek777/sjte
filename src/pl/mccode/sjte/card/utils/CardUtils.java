7c7254cc77840baf2e1b1169fc2edbc897005318
pietrek777 <pietrek777@gmail.com>
2017-01-20 23:17:29 +0100

    Add card shuffling function

---
+++ src/pl/mccode/sjte/card/utils/CardUtils.java
@@ -0,0 +1,22 @@
+package pl.mccode.sjte.card.utils;
+
+import pl.mccode.sjte.card.Card;
+import pl.mccode.sjte.card.meta.CardDeck;
+import pl.mccode.sjte.card.meta.CardSuit;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CardUtils {
+	public static List<Card> getShuffledCards(){
+		List<Card> list = new ArrayList<>();
+		for(CardSuit cardSuit: CardSuit.values()){
+			for(CardDeck cardDeck: CardDeck.values()){
+				list.add(new Card(cardDeck, cardSuit));
+			}
+		}
+		Collections.shuffle(list);
+		return list;
+	}
+}
